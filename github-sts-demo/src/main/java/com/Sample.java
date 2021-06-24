42e3605526191cf10e70d937c2e64daf638c6c0e
Gouri11 <81665449+Gouri11@users.noreply.github.com>
2021-06-24 19:18:18 +0530

    Update Sample.java

--- github-sts-demo/src/main/java/com/Sample.java
+++ github-sts-demo/src/main/java/com/Sample.java
@@ -4,7 +4,7 @@ public class Sample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.print("my first project");
+System.out.print("my first project of git");
 	}
 
 }
